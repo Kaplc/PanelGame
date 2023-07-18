b7937accadffeca4751724c40945e77d02a6bb22
Kaplc <755536175@qq.com>
2023-07-18 19:58:16 +0800

    面板基类

---
+++ Assets/Scripts/UI/BasePanel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BasePanel<T> : MonoBehaviour where T : class
+{
+    private T instance;
+    public T Instance => instance;
+    
+    protected virtual void Awake()
+    {
+        instance = this as T;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Init();
+    }
+
+    public virtual void Init()
+    {
+        
+    }
+
+    public virtual void Show()
+    {
+        gameObject.SetActive(true);
+    }
+
+    public virtual void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
